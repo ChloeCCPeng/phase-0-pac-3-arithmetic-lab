1112cd9bf220b45529daad985db20fdc0b5c3f6a
Chloe Peng <chloepeng331@icloud.com>
2022-01-08 14:45:36 -0500

    Assignment completed

---
+++ index.js
@@ -0,0 +1,58 @@
+const numb = 0;
+
+function add(numb) {
+    numb+=8;
+    console.log(numb);
+}
+
+function subtract(numb) {
+    numb-=2;
+    console.log(numb);
+}
+
+function multiply(numb) {
+    numb*=10;
+    console.log(numb);
+}
+
+function divide(numb) {
+    numb/=3;
+    console.log(numb);
+}
+
+function add (a,b){
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+} 
+
+function multiply(a,b){
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function increment (n){
+    return n+=1;
+}
+
+function decrement(n) {
+    return n-=1;
+}
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    return parseInt(n, 10);
+}
+
+function preserveDecimal(n) {
+    parseInt(n, 10);
+    return parseFloat (n);
+}
